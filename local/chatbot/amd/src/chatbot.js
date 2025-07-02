58cc19193f995c3cd93aac5bd01e613459824605
rashwin88 <raviashwin87@gmail.com>
2025-07-02 20:28:41 +0530

    Display context and sender names in chat

--- local/chatbot/amd/src/chatbot.js
+++ local/chatbot/amd/src/chatbot.js
@@ -1,12 +1,14 @@
 define(['jquery', 'core/templates'], function($, Templates) {
     function init(opts) {
         var userid = opts.userid;
+        var username = opts.username;
         var coursename = opts.coursename;
         var iconUrl = M.util.image_url('logo', 'local_chatbot');
 
         Templates.render('local_chatbot/chatbox', {iconurl: iconUrl}).done(function(html) {
             $('body').append(html);
             Templates.runTemplateJS(html);
+            $('#chatbot-context').text(username + ' (' + userid + ') - ' + coursename);
             setupEvents();
         });
 
@@ -38,7 +40,8 @@ define(['jquery', 'core/templates'], function($, Templates) {
         }
 
         function addMessage(sender, text) {
-            Templates.render('local_chatbot/message', {sender: sender, text: text}).done(function(html) {
+            var sendername = sender === 'bot' ? 'sid' : username;
+            Templates.render('local_chatbot/message', {sender: sender, text: text, sendername: sendername}).done(function(html) {
                 $('#chatbot-messages').append(html);
                 Templates.runTemplateJS(html);
                 var container = $('#chatbot-messages')[0];
@@ -52,12 +55,19 @@ define(['jquery', 'core/templates'], function($, Templates) {
                 'Formulating a response...',
                 'Referencing course modules...'
             ];
-            var delay = 0;
-            steps.forEach(function(step){
-                setTimeout(function(){ addMessage('bot', step); }, delay);
-                delay += 1000;
-            });
-            setTimeout(callback, delay);
+            var $thinking = $('#chatbot-thinking');
+            var index = 0;
+            function next() {
+                if (index < steps.length) {
+                    $thinking.text(steps[index]);
+                    index++;
+                    setTimeout(next, 1000);
+                } else {
+                    $thinking.text('');
+                    callback();
+                }
+            }
+            next();
         }
     }
 
