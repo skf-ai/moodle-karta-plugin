98a8f554f317affd76a8cb546171d63c88658901
rashwin88 <raviashwin87@gmail.com>
2025-08-26 11:49:42 +0530

    Update chatbot version to 2025070222

--- local/chatbot/version.php
+++ local/chatbot/version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_chatbot';
-$plugin->version = 2025070219;
+$plugin->version = 2025070222;
 $plugin->requires = 2019052000; // Moodle 3.7.
